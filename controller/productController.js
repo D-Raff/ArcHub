a1921933255cd848bf0a14109462ce89c9212d8b
D-Raff <damonraffels@gmail.com>
2024-03-05 12:09:23 +0200

    created product model and controller

---
+++ controller/productController.js
@@ -0,0 +1,62 @@
+import express from "express";
+import bodyParser from "body-parser";
+import { products } from "../model/index.js";
+
+const productRouter = express.Router()
+
+//fetch all products
+productRouter.get('/', (req, res)=>{
+    try {
+        products.fetchProducts(req, res)
+    } catch (e) {
+        res.json({
+            status: res.statusCode,
+            msg: "An error occured when attempting to fetch products"
+        })
+    }
+})
+//fetch a single product
+productRouter.get('/:id', (req, res)=>{
+    try {
+        products.fetchProduct(req, res)
+    } catch (e) {
+        res.json({
+            status: res.statusCode,
+            msg: "An error occured when attempting to fetch this product"
+        })
+    }
+})
+//add a new product
+productRouter.post('/add', bodyParser.json(), (req, res)=>{
+    try {
+        products.addProduct(req, res)
+    } catch (e) {
+        res.json({
+            status: res.statusCode,
+            msg: "Failed to add a new product at this time. Please try again later."
+        })
+    }
+})
+productRouter.patch('/update/:id', bodyParser.json(), (req, res)=>{
+    try {
+        products.updateProduct(req, res)
+    } catch (e) {
+        res.json({
+            status: res.statusCode,
+            msg: "This product was updated"
+        })
+    }
+})
+productRouter.delete('/delete/:id', bodyParser.json(), (req, res)=>{
+    try {
+        products.deleteProduct(req, res)
+    } catch (e) {
+        res.json({
+            status: res.statusCode,
+            msg: "deleted product"
+        })
+    }
+})
+export{
+    productRouter, express
+}
---
+++ model/Products.js
@@ -0,0 +1,87 @@
+import { Connection as db } from "../config/index.js";
+
+class Products{
+    fetchProducts(req, res){
+        const qry = `
+        SELECT ProductID, ProductName, Category, Price, ProdImg, ProdDesc, Stock
+        FROM Products;
+        `
+        db.qry(qry, (err, results)=> {
+            if (err) throw err
+            res.json({
+                status: res.statuscode,
+                results
+            })
+        })
+    }
+    fetchProduct(req, res){
+        const qry = `
+        SELECT ProductID, ProductName, Category, Price, ProdImg, ProdDesc, Stock
+        FROM Products
+        WHERE prodID = ${req.params.id};
+        `
+        db.query(qry, (err, result)=>{
+            if(err) throw err
+            res.json({
+                status: res.statusCode,
+                result: result[0]
+            })
+        })
+    }
+    async addProduct(req, res){
+        const qry = `
+        INSERT INTO Products SET ?;
+        `
+        db.query(qry, [req.body], (err)=>{
+            if(err) throw err
+            res.json({
+                status: res.statusCode,
+                msg: "New product was added"
+            })
+        })
+    }
+    async addProduct(req, res){
+        const qry = `
+        INSERT INTO Products SET ?;
+        `
+        db.query(qry, [req.body], (err)=>{
+            if(err) throw err
+            res.json({
+                status: res.statusCode,
+                msg: "New product was added"
+            })
+        })
+    }
+    async deleteProduct(req, res){
+        const qry = `
+        DELETE FROM Products
+        WHERE prodID = ${req.params.id};
+        `
+        db.query(qry, [req.body], (err)=>{
+            if(err) throw err
+            res.json({
+                status: res.statusCode,
+                msg: "This product was deleted"
+            })
+        })
+    }
+    async updateProduct(req, res){
+        let data = req.body
+        const qry = `
+        UPDATE Products
+        SET ?
+        WHERE prodID = ${data.prodID}
+        `
+
+        db.query(qry, [data], (err)=>{
+            if (err) throw err
+            res.json({
+                status: res.statusCode,
+                msg: "This Product was updated"
+            })
+        })
+    }
+}
+export{
+    Products
+}
