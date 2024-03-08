eefc79b0327eda0fa6affb3e6b005ca289afb07d
D-Raff <damonraffels@gmail.com>
2024-03-08 16:19:23 +0200

    front-end stuff

--- src/store/index.js
+++ src/store/index.js
@@ -1,13 +1,133 @@
 import { createStore } from 'vuex'
+import axios from "axios";
+import sweet from "sweetalert";
+import { useCookies } from "vue3-cookies";
+const { cookies } = useCookies();
+import router from "@/router";
+import AuthenticateUser from "../service/UserAuthentication";
+const db = "https://archub-471i.onrender.com/";
 
 export default createStore({
   state: {
+    users: null,
+    user: null,
+    products: null,
+    product: null
   },
   getters: {
   },
   mutations: {
+    setUsers(state, value) {
+      state.users = value;
+    },
+    setUser(state, value) {
+      state.user = value;
+    },
+    setProducts(state, value) {
+      state.products = value;
+    },
+    setProduct(state, value) {
+      state.product = value;
+    },
   },
   actions: {
+    async register(context, payload) {
+      try {
+        let msg = (await axios.post(`${db}users/register`, payload)).data;
+          context.dispatch("fetchUsers");
+          sweet({
+            title: "Sign Up",
+            text: msg,
+            icon: "success",
+            timer: 4000,
+          });
+          location.reload()
+          router.push({ name: "login" });
+      } catch (e) {
+        sweet({
+          title: "Error",
+          text: "Please try again at a different time",
+          icon: "error",
+          timer: 4000,
+        });
+      }
+    },
+    async fetchUsers(context) {
+      try {
+        let results = (await axios.get(`${db}users`)).data;
+        if (results) {
+          context.commit("setUsers", results);
+        }
+      } catch (e) {
+        sweet({
+          title: "Error",
+          text: "An error occured while retrieving Users",
+          icon: "error",
+          timer: 2000,
+        });
+      }
+    },
+    async fetchUser(context, payload) {
+      try {
+        let result = (await axios.get(`${db}users/${payload.id}`)).data;
+        if (result) {
+          context.commit("setUser", result);
+        } else {
+          sweet({
+            title: "Retrieving a single user",
+            text: "User not found",
+            icon: "info",
+            timer: 4000,
+          });
+        }
+      } catch (e) {
+        sweet({
+          title: "Error",
+          text: "this user was not found.",
+          icon: "error",
+          timer: 4000,
+        });
+      }
+    },
+    async login(context, payload) {
+      try {
+        const { msg, token, result } = (
+          await axios.post(`${db}users/login`, payload)
+        ).data;
+        if (result) {
+          context.commit("setUser", { msg, result });
+          alert(result)
+          cookies.set("VerifiedUser", {
+            msg,
+            token,
+            result,
+          });
+          AuthenticateUser.applyToken(token);
+          sweet({
+            title: msg,
+            text: `Welcome, 
+          ${result?.firstName} ${result?.lastName}`,
+            icon: "success",
+            timer: 2000,
+          });
+          router.push({ name: "/" });
+        } else {
+          sweet({
+            title: "info",
+            text: msg,
+            icon: "info",
+            timer: 4000,
+          });
+        }
+      } catch (e) {
+        sweet({
+          title: "Error",
+          text: "Failed to login.",
+          icon: "error",
+          timer: 4000,
+        });
+      }
+    },
   },
   modules: {
   }
---
+++ src/views/LoginView.vue
@@ -0,0 +1,115 @@
+<template>
+    <div class="Login">
+        <div class="login-box">
+            <form @submit.prevent>
+                <div class="user-box">
+                    <input v-model="emailAdd" type="text" name="" placeholder="EmailAddress">
+                </div>
+                <div class="user-box">
+                    <input v-model="password" type="password" name="" placeholder="Password">
+                </div>
+                    <button class="btn-log" @click="login()"  type="submit">
+                        Login
+                        <span class="line"></span>
+                    </button>
+            </form>
+        </div>
+    </div>
+</template>
+<script>
+export default {
+    data() {
+        return {
+            emailAdd: "",
+            password: ""
+        }
+    },
+    methods: {
+        login(){
+            this.data = { emailAdd: this.emailAdd, password: this.password}
+            this.$store.dispatch('login', this.data)
+        }
+    },
+
+}
+</script>
+<style>
+.login-box {
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    width: 400px;
+    padding: 40px;
+    transform: translate(-50%, -50%);
+    background: rgba(24, 20, 20, 0.987);
+    box-sizing: border-box;
+    box-shadow: 0 15px 25px rgba(0, 0, 0, .6);
+    border-radius: 10px;
+}
+
+.user-box {
+    position: relative;
+}
+
+.user-box input {
+    width: 100%;
+    padding: 10px 0;
+    font-size: 16px;
+    color: #fff;
+    margin-bottom: 30px;
+    border: none;
+    border-bottom: 1px solid #B01515;
+    outline: none;
+    background: transparent;
+}
+
+.btn-log {
+    position: relative;
+    display: inline-block;
+    padding: 10px 20px;
+    color: #B01515;
+    font-size: 16px;
+    text-decoration: none;
+    text-transform: uppercase;
+    overflow: hidden;
+    transition: .5s;
+    margin-top: 40px;
+    letter-spacing: 4px;
+    background: transparent;
+    border: none;
+}
+
+.btn-log:hover {
+    background: #14c8f0;
+    color: #B01515;
+    border-radius: 5px;
+    box-shadow: 0 0 5px #14c8f0,
+        0 0 25px #14c8f0,
+        0 0 50px #14c8f0,
+        0 0 100px #14c8f0;
+}
+
+.line {
+    position: absolute;
+    display: block;
+    bottom: 2px;
+    left: -100%;
+    width: 100%;
+    height: 2px;
+    background: linear-gradient(90deg, transparent, #14c8f0);
+    animation: btn-anim1 2s linear infinite;
+}
+
+@keyframes btn-anim1 {
+    0% {
+        left: -100%;
+    }
+
+    50%,
+    100% {
+        left: 100%;
+    }
+}
+
+
+</style>
