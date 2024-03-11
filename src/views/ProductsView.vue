3dd018e48ca0b636826aad9c8a3336e0a5104144
D-Raff <damonraffels@gmail.com>
2024-03-11 16:35:20 +0200

    building product page

--- src/views/ProductsView.vue
+++ src/views/ProductsView.vue
@@ -1,11 +1,22 @@
 <template>
-    <div class="Products">
-        
+    <div class="Products container-fluid">
+        <div v-if="products" class="container">
+            <card v-for="product in products" :key="product.productID">
+                <template #Title>{{ product.ProductName }}</template>
+                <template #desc>{{ product.ProdDesc }}</template>
+            </card>
+        </div>
+
     </div>
 </template>
 <script>
+import card from '@/components/card.vue';
+
 export default {
     name: "ProductsView",
+    components:{
+        card
+    },
     data() {
         return {
             cartPayload:{
@@ -19,8 +30,16 @@ export default {
         addtoCart() {
             this.data = { productID: this.productID, userID: this.userID }
             this.$store.dispatch('addToCart', this.data);
-        },
+        }
     },
+    computed: {
+        products() {
+            return this.$store.state.products
+        }
+    },
+    mounted() {
+        this.$store.dispatch('fetchProducts')
+    }
 }
 </script>
 <style>
